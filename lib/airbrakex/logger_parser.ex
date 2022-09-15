83abb7da146f800bf7fd4f90ff51660d17f2618e
Creighton Medley <creighton.medley@lexmark.com>
2022-09-15 10:27:56 -0400

    remove genserver pid from message so the same liveview errors are not unique

--- lib/airbrakex/logger_parser.ex
+++ lib/airbrakex/logger_parser.ex
@@ -5,6 +5,8 @@ defmodule Airbrakex.LoggerParser do
   @exception_header_regex ~r/an exception was raised:/
 
   def parse(msg) do
+    msg = Regex.replace(~r/GenServer.*>/, msg, "GenServer error")
+
     type = Regex.named_captures(@type_regex, msg)["type"]
     lines = String.split(msg, "\n")
 
